7890c0364fe6072f1fd810f5ebbe9686844811f6
rrijal53 <rijalroshan94@gmail.com>
2021-09-13 07:50:10 +0545

    removed demo

--- MVCDemo.java
+++ MVCDemo.java
@@ -1,9 +1,6 @@
 class MVCDemo {
     public static void main(String[] args) {
         Employee e = new Employee();
-        EmployeeView v = new EmployeeView();
-        EmployeeController controller = new EmployeeController(e, v);
-        controller.setEmployeeDetail("Roshan", "Rijasldsd", 1111111);
-        controller.getEmployeeName();
+       
     }
 }
