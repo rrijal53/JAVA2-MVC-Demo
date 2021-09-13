d4b971a06a5a94b1cd6f9554d61961655b5a2433
rrijal53 <rijalroshan94@gmail.com>
2021-09-13 07:49:00 +0545

    print setdata message

--- EmployeeController.java
+++ EmployeeController.java
@@ -1,23 +1,25 @@
-class EmployeeController{
-   Employee mModel;
-   EmployeeView mView;
+class EmployeeController {
+    Employee mModel;
+    EmployeeView mView;
 
-     public EmployeeController(Employee mModel, EmployeeView mView) {
-           this.mModel = mModel;
-           this.mView = mView;
-       }
-
-
-    public void getEmployeeName(){
-          
-           mView.showLoading();
-           mView.showEmployeeName( mModel.getName());
+    public EmployeeController(Employee mModel, EmployeeView mView) {
+        this.mModel = mModel;
+        this.mView = mView;
     }
 
-   public void setEmployeeDetail(String name, String address, int salary){
+
+    public void getEmployeeName() {
+
+        mView.showLoading();
+        mView.showEmployeeName(mModel.getName());
+
+    }
+
+    public void setEmployeeDetail(String name, String address, int salary) {
 
         mModel.setName(name);
         mModel.setAddress(address);
         mModel.setSalary(salary);
-   }
+        System.out.println("Setting employee detail");
+    }
 }
